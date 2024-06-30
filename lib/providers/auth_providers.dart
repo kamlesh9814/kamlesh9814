36beb6f750c548dfe70ddbba6202cde8105c5622
kamlesh sharma <you@example.com>
2024-06-30 11:43:10 +0545

    first changes

--- lib/providers/auth_providers.dart
+++
@@ -1,5 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final userStream = StreamProvider.autoDispose(
-        (ref) => FirebaseAuth.instance.authStateChanges());
